6f7478bd7d26982f3978203e9246ff5fa3e05e78
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2020-11-05 13:18:29 +0100

    Revert "Revert "added uncommented code for localization""
    This reverts commit 2bcd843219306b5465863bc6408b6a3dd431d5c3.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,18 @@
 console.log("language: ", window.navigator.language);
+
+/*function localize (language) {
+  if (['sv'].includes(language)) {
+    console.log("includes sv");
+    let lang = ':lang(' + language + ')';
+    let hide = '[lang]:not(' + lang + ')';
+    document.querySelectorAll(hide).forEach(function (node) {
+      node.style.display = 'none';
+    });
+    let show = '[lang]' + lang;
+    document.querySelectorAll(show).forEach(function (node) {
+      node.style.display = 'unset';
+    });
+  }
+}
+
+localize(window.navigator.language);*/
