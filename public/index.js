f66eaf5005cea11ccaac346af9a69162c3b68c20
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2020-11-05 14:02:23 +0100

    changed to language from id

--- public/index.js
+++ public/index.js
@@ -2,18 +2,18 @@ console.log("language: ", window.navigator.language);
 
 if (window.navigator.language == 'sv-SE') {
   console.log('svenska');
-  document.querySelectorAll("#sv").forEach(function (node) {
+  document.querySelectorAll('[lang="sv"]').forEach(function (node) {
     node.hidden = false;
   });
-  document.querySelectorAll("#en").forEach(function (node) {
+  document.querySelectorAll('[lang="en"]').forEach(function (node) {
     node.hidden = true;
   });
 } else {
   console.log('engelska');
-  document.querySelectorAll("#sv").forEach(function (node) {
+  document.querySelectorAll('[lang="sv"]').forEach(function (node) {
     node.hidden = true;
   });
-  document.querySelectorAll("#en").forEach(function (node) {
+  document.querySelectorAll('[lang="en"]').forEach(function (node) {
     node.hidden = false;
   });
 }
